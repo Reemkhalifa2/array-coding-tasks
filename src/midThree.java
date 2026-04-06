b03618779c84d5258d7374079bd4b3e9c378b90f
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-06 12:09:20 +0400

    Task 8: midThree

---
+++ src/midThree.java
@@ -0,0 +1,5 @@
+public class midThree {
+    public static Integer[] midThree(Integer [] nums){
+        return new Integer[] {nums[nums.length/2-1], nums[nums.length/2], nums[nums.length/2+1]};
+    }
+}
