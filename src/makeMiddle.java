f40b3761e736951ea5533a82ae7610b8e33b3eea
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-06 12:05:23 +0400

    Task 7 : makeMiddle

--- src/makeMiddle.java
+++ src/makeMiddle.java
@@ -1,5 +1,6 @@
 public class makeMiddle {
     public static Integer[] makeMiddle(Integer[] nums){
+        //Task 7
         return new Integer[] {nums[nums.length/2-1], nums[nums.length/2]};
     }
 }
