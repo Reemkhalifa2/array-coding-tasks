2a6310d2f5708706c11a8db6f0e7782056a0b084
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-06 10:39:00 +0400

    Task 2 solution

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,29 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(firstLast6(new Integer[]{1, 2, 6}));
+        Integer[] array1 = {1,2,4};
+        Integer[] array2 = {7,3};
+        System.out.println(commonEnd(array1, array2));
 
         }
 
-    //Q1
+
+    //Task1
     public static Boolean firstLast6 (Integer[] nums){
         Boolean found = false;
         for(Integer num :nums){
             if (num == 6){
+                found = true;
+            }
+        }
+        return found;
+    }
+
+    //Task 2
+    public static Boolean commonEnd(Integer[] array1, Integer[] array2){
+        Boolean found = false;
+        if(array1.length > 0 && array2.length > 0) {
+            if(array1[0].equals(array2[0]) || array1[array1.length - 1].equals(array2[array2.length - 1])){
                 found = true;
             }
         }
