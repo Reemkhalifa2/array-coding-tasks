9037a3de0558145f80b8e45021656a5bd53c6586
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-06 14:37:19 +0400

    Task 14 : start1

---
+++ src/start1.java
@@ -0,0 +1,12 @@
+public class start1 {
+    public static Integer start1(Integer [] a, Integer [] b){
+        Integer count =0;
+        if(a[0]==1){
+            count+=1;
+        }
+        if(b[0]==1){
+            count+=1;
+        }
+        return  count;
+    }
+}
