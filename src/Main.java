ab9f9f38a1329ad072bc9221b270ece34bfd25ae
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-06 11:06:40 +0400

    resolve Task 1 : firstLast6

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println(firstLast6(new Integer[]{1, 2, 6}));
-        Integer[] array1 = {1,2,4};
-        Integer[] array2 = {7,3};
-        System.out.println(commonEnd(array1, array2));
-
         }
-
-
-    //Task1
+    //Task 1: firstLast6
     public static Boolean firstLast6 (Integer[] nums){
-        Boolean found = false;
-        for(Integer num :nums){
-            if (num == 6){
-                found = true;
-            }
-        }
-        return found;
+        return nums[0] == 6 || nums[nums.length-1] == 6;
     }
 
-    //Task 2
-    public static Boolean commonEnd(Integer[] array1, Integer[] array2){
-        Boolean found = false;
-        if(array1.length > 0 && array2.length > 0) {
-            if(array1[0].equals(array2[0]) || array1[array1.length - 1].equals(array2[array2.length - 1])){
-                found = true;
-            }
-        }
-        return found;
-    }
 
-    }
+
+
+}
