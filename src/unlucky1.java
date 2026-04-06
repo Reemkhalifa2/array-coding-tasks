346e61b4ffe55fdd102f634a60099fbf3a57403a
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-06 12:20:48 +0400

    Task 9 : unlucky1

---
+++ src/unlucky1.java
@@ -0,0 +1,8 @@
+public class unlucky1 {
+    public static Boolean unlucky1 (Integer[]nums){
+        if (nums.length < 2) {
+            return false;
+        }
+        return (nums[0]==1 && nums[1]==3 || nums[1]==1 && nums[2]==3 || nums[nums.length-2]==1 && nums[nums.length-1]==3);
+    }
+}
