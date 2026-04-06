2a906d7c96a21b30927638beac6b50e3d2e08fd5
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-06 11:57:25 +0400

    update main

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,15 @@ public class Main {
         for(Integer n : Reslut){
             System.out.println(n);
         }
+        Integer [] middle = middleWay.middleWay(new Integer[]{1, 2, 6},new Integer[]{1,4,8});
+        for(Integer n : middle){
+            System.out.println(n);
+        }
+        System.out.println(no23.no23(new Integer[]{1,1}));
+        Integer [] r = fix23.fix23(new Integer[]{1, 2, 3});
+        for(Integer n : r){
+            System.out.println(n);
+        }
 
     }
 
