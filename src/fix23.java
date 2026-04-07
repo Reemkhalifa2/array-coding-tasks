95b42f254f4a40d2ee16b6a088707b892a7bba68
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-07 09:01:59 +0400

    Task 6 :resolve fixe23

--- src/fix23.java
+++ src/fix23.java
@@ -1,10 +1,9 @@
 public class fix23 {
     public static Integer[] fix23(Integer[] nums){
-        for(Integer num : nums){
-            if(nums[num]==2 && nums[num+1]==3){
-                nums[2] = 0;
-            }
-        }
-        return nums;
+        if (nums[0] == 2 && nums[1] == 3)
+            nums[1] = 0;
+        if (nums[1] == 2 && nums[2] == 3)
+            nums[2] = 0;
+        return new Integer[] {nums[0],nums[1],nums[2]};
     }
 }
