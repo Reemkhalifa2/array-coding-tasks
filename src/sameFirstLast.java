2dbdf904e4342b77c3e55b90bed624920ca11537
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-07 09:15:07 +0400

    Task 10: resolve sameFirstLast

--- src/sameFirstLast.java
+++ src/sameFirstLast.java
@@ -1,6 +1,6 @@
 public class sameFirstLast {
     public static Boolean sameFirstLast(Integer [] nums){
-        return nums.length >=1 && nums[0].equals(nums[nums.length-1]) ;
+        return nums.length >=1 && nums[0] == (nums[nums.length-1]) ;
 
     }
 }
